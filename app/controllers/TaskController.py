23a84beac9feff09ded21c5a145353c7d5ee605e
unknown <threezinedine@gmail.com>
2023-01-03 15:10:03 +0700

    pass test for querying wrong username

--- app/controllers/TaskController.py
+++ app/controllers/TaskController.py
@@ -1,5 +1,8 @@
 from sqlalchemy.orm import Session
-from typing import List
+from typing import (
+    List,
+    Union,
+)
 from datetime import datetime
 
 from databases.models import Task
@@ -11,9 +14,14 @@ class TaskController:
         self.session = session
         self.user_controller = user_controller
 
-    def get_all_tasks_by_username(self, username: str) -> List[Task]:
+    def get_all_tasks_by_username(self, username: str) -> Union[List[Task], None]:
+        tasks = None
         user = self.user_controller.get_user_by_name(username)
-        return user.tasks
+
+        if user is not None:
+            tasks = user.tasks
+
+        return tasks
 
     def create_new_task_by_username(self, username: str, 
                 taskName: str, 
